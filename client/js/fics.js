6dc6265c64f1276db839bb388deab7d54ba3a22b
brittonf <brittonfarrar@gmail.com>
2020-11-28 15:19:54 +0000

    better warroom theme; cheap bug handling fix for busted game.chess object

--- client/js/fics.js
+++ client/js/fics.js
@@ -16,32 +16,18 @@
 #define BLK_VARIABLES 143
 #define BLK_WITHDRAW 147
 */
-default_themes = [{
-"name":"cop",
-"light_rgba":{"r":255,"g":255,"b":255,"a":0.09},
-"dark_rgba":{"r":255,"g":255,"b":255,"a":0},"texture":"brushed-copper.jpg","white_pieces":"alpha","black_pieces":"alpha"},{
-"name":"leav",
-"dark_rgba":{"r":0,"g":0,"b":0,"a":0.71},
-"light_rgba":{"r":255,"g":255,"b":255,"a":0.56},"texture":"Nature-Leaves-Background-70-625x500.jpg","white_pieces":"chesscom","black_pieces":"chess24"},{
-"name":"fire",
-"dark_rgba":{"r":0,"g":0,"b":0,"a":0.74},
-"light_rgba":{"r":253.57199999999997,"g":132.59259171619996,"b":33.141860399999985,"a":0.72},"texture":"Fire-Flames-Background-11-625x468.jpg","white_pieces":"chesscom","black_pieces":"chesscom"},{
-"name":"hunjies",
-"dark_rgba":{"r":48.432867795674994,"g":101.8215,"b":21.718525949999993,"a":0.81},
-"light_rgba":{"r":245.463,"g":244.67996075685002,"b":241.70741610000002,"a":0.79},"texture":"Money-Background-17-625x625.jpg","white_pieces":"uscf","black_pieces":"uscf"},{
-"name":"met",
-"dark_rgba":{"r":0,"g":0,"b":0,"a":0},
-"light_rgba":{"r":226.63988810459998,"g":250.002,"b":54.77543819999999,"a":0.46},"texture":"metal-plate-texture_1048-2443.jpg","white_pieces":"chesscom_wood","black_pieces":"chesscom"},{
-"name":"wood",
-"dark_rgba":{"r":91.31550000000001,"g":28.09777935,"b":28.09777935,"a":0.75},
-"light_rgba":{"r":176.12435600010002,"g":238.4505,"b":103.1059962,"a":0.2},"texture":"antique-wooden-planks-texture_1232-824.jpg","white_pieces":"chesscom","black_pieces":"alpha"},{
-"name":"wawa",
-"dark_rgba":{"r":2.8474999999998563,"g":0,"b":255,"a":0.43},
-"light_rgba":{"r":239.0115,"g":235.93176511905,"b":224.2405893,"a":0.07},"texture":"blue-rippled-water-background-in-swimming-pool_1373-193.jpg","white_pieces":"uscf","black_pieces":"chess24"},{
-"name":"map",
-"light_rgba":{"r":250.002,"g":242.6519412,"b":242.6519412,"a":0.76},
-"dark_rgba":{"r":0,"g":148.49500000000006,"b":255,"a":0.39},"texture":"11.jpg","white_pieces":"alpha","black_pieces":"chess24"}]
-
+default_themes = [
+    {"name":"cop","light_rgba":{"r":255,"g":255,"b":255,"a":0.09},"dark_rgba":{"r":255,"g":255,"b":255,"a":0},"texture":"brushed-copper.jpg","white_pieces":"alpha","black_pieces":"alpha"},
+    {"name":"fire","dark_rgba":{"r":0,"g":0,"b":0,"a":0.74},"light_rgba":{"r":253.57,"g":132.59,"b":33.14,"a":0.72},"texture":"Fire-Flames-Background-11-625x468.jpg","white_pieces":"chesscom","black_pieces":"chesscom"},
+    {"name":"hunjies","dark_rgba":{"r":48.43,"g":101.82,"b":21.71,"a":0.81},"light_rgba":{"r":245.46,"g":244.67,"b":241.70,"a":0.79},"texture":"Money-Background-17-625x625.jpg","white_pieces":"uscf","black_pieces":"uscf"},
+    {"name":"leaves","dark_rgba":{"r":0,"g":0,"b":0,"a":0.71},"light_rgba":{"r":255,"g":255,"b":255,"a":0.56},"texture":"Nature-Leaves-Background-70-625x500.jpg","white_pieces":"chesscom","black_pieces":"chess24"},
+    {"name":"medieval","dark_rgba":{"r":0,"g":0,"b":0,"a":0.55},"light_rgba":{"r":243.98,"g":228.07,"b":195.99,"a":0.64},"texture":"kentmap.jpg","white_pieces":"leipzig","black_pieces":"leipzig"},
+    {"name":"metal","dark_rgba":{"r":0,"g":0,"b":0,"a":0},"light_rgba":{"r":226.63,"g":250.002,"b":54.77,"a":0.46},"texture":"metal-plate-texture_1048-2443.jpg","white_pieces":"chesscom_wood","black_pieces":"chesscom"},
+    {"name":"royal","dark_rgba":{"r":116.17,"g":38.98,"b":255,"a":0.71},"light_rgba":{"r":242.47,"g":205.57,"b":65.49,"a":0.55},"texture":"Money-Background-19-625x468.jpg","white_pieces":"wikipedia","black_pieces":"alpha"},
+    {"name":"stone","dark_rgba":{"r":145.60,"g":2.82,"b":2.82,"a":0.372},"light_rgba":{"r":255,"g":200.7105,"b":200.7105,"a":0.47},"texture":"Stone-Tiles-Background-22-625x625.jpg","white_pieces":"symbol","black_pieces":"chess24"},
+    {"name":"warroom","dark_rgba":{"r":0,"g":0,"b":0,"a":0.6},"light_rgba":{"r":46.42,"g":254.39,"b":0,"a":0.63},"texture":"design_digital_map3.jpg","white_pieces":"chesscom","black_pieces":"chess24"},
+    {"name":"wawa","dark_rgba":{"r":2.847,"g":0,"b":255,"a":0.43},"light_rgba":{"r":239.01,"g":235.93,"b":224.24,"a":0.07},"texture":"blue-rippled-water-background-in-swimming-pool_1373-193.jpg","white_pieces":"uscf","black_pieces":"chess24"},
+    {"name":"wood","dark_rgba":{"r":91.31,"g":28.09,"b":28.09,"a":0.75},"light_rgba":{"r":176.12,"g":238.45,"b":103.1,"a":0.2},"texture":"antique-wooden-planks-texture_1232-824.jpg","white_pieces":"chesscom","black_pieces":"alpha"}]
 
 
 
@@ -157,62 +143,64 @@ ficswrap.on("result", function(msg) {
         var game_num = ficsobj.s12.game_num;
         var game = gamemap.get(game_num);
 
-        if (game.chess.history().length != game.getMoveIndexFromS12() + 1) {
-            console.log('chess says '+game.chess.history().length + ' but s12 says ' + game.getMoveIndexFromS12() +', doing nothing');
-            //ficswrap.emit('command', 'moves '+game_num);
-        } else {
-            var move_info = game.chess.move(ficsobj.s12.move_note_short, {sloppy:true});
-            if (move_info) {
-                if (game.chess.in_check()) {
-                    soundmap.checks[Math.floor(Math.random() * soundmap.checks.length)].play();
-                } else if (['n','b','k','q','p'].includes(move_info.flags)) {
-                    soundmap.moves[Math.floor(Math.random() * soundmap.moves.length)].play();
-                } else {
-                    soundmap.captures[Math.floor(Math.random() * soundmap.captures.length)].play();
+        if (game.chess) { 
+            if (game.chess.history().length != game.getMoveIndexFromS12() + 1) {
+                console.log('chess says '+game.chess.history().length + ' but s12 says ' + game.getMoveIndexFromS12() +', doing nothing');
+                //ficswrap.emit('command', 'moves '+game_num);
+            } else {
+                var move_info = game.chess.move(ficsobj.s12.move_note_short, {sloppy:true});
+                if (move_info) {
+                    if (game.chess.in_check()) {
+                        soundmap.checks[Math.floor(Math.random() * soundmap.checks.length)].play();
+                    } else if (['n','b','k','q','p'].includes(move_info.flags)) {
+                        soundmap.moves[Math.floor(Math.random() * soundmap.moves.length)].play();
+                    } else {
+                        soundmap.captures[Math.floor(Math.random() * soundmap.captures.length)].play();
+                    }
+
+                    game.s12 = ficsobj.s12;
+                    if (['1-0','0-1','1/2-1/2'].indexOf(game.result) === -1) {
+                        runClock(game_num);
+                    }
+
+                    var new_move_index = game.chess.history().length - 1;
+                
+                    game.movetimes[new_move_index] = ficsobj.s12.move_time;
+                    game.fens[new_move_index] = game.chess.fen().split(/\s+/)[0];
+                    appendToMoveList(game_num, new_move_index);
+
+
+                    var whose_move = ['w','b'][new_move_index % 2];
+
+                    if (whose_move === game.human_color && !game.premove) { goToMove(game_num, new_move_index, animate=false) }
+                    else { goToMove(game_num, new_move_index, animate=true); }
                 }
 
-                game.s12 = ficsobj.s12;
-                if (['1-0','0-1','1/2-1/2'].indexOf(game.result) === -1) {
-                    runClock(game_num);
-                }
+                if (ficsobj.s12.my_rel === '1' && game.premove) {
+                    var source = game.premove.from;
+                    var target = game.premove.to;
+                    var piece = game.premove.piece;
+                    var mv = { from: source, to: target };
 
-                var new_move_index = game.chess.history().length - 1;
-            
-                game.movetimes[new_move_index] = ficsobj.s12.move_time;
-                game.fens[new_move_index] = game.chess.fen().split(/\s+/)[0];
-                appendToMoveList(game_num, new_move_index);
-
-
-                var whose_move = ['w','b'][new_move_index % 2];
-
-                if (whose_move === game.human_color && !game.premove) { goToMove(game_num, new_move_index, animate=false) }
-                else { goToMove(game_num, new_move_index, animate=true); }
-            }
-
-            if (ficsobj.s12.my_rel === '1' && game.premove) {
-                var source = game.premove.from;
-                var target = game.premove.to;
-                var piece = game.premove.piece;
-                var mv = { from: source, to: target };
-
-                if (/[18]$/.test(target) && /[pP]/.test(piece)) {
-                    var choices = game.chess.moves({verbose:true});
-                    for (var i=0; i<choices.length; i++) {
-                        m = choices[i];
-                        if (m.from === source && m.to === target) {
-                            mv.promotion = 'q';
-                            break;
+                    if (/[18]$/.test(target) && /[pP]/.test(piece)) {
+                        var choices = game.chess.moves({verbose:true});
+                        for (var i=0; i<choices.length; i++) {
+                            m = choices[i];
+                            if (m.from === source && m.to === target) {
+                                mv.promotion = 'q';
+                                break;
+                            }
                         }
                     }
-                }
 
-                highlightSquares($('#board_'+game_num), 'red', clear=true);
-                game.premove = null;
+                    highlightSquares($('#board_'+game_num), 'red', clear=true);
+                    game.premove = null;
 
-                var valid_move = game.chess.move(mv);
-                if (valid_move) {
-                    game.chess.undo();
-                    ficswrap.emit('command',valid_move.san);
+                    var valid_move = game.chess.move(mv);
+                    if (valid_move) {
+                        game.chess.undo();
+                        ficswrap.emit('command',valid_move.san);
+                    }
                 }
             }
         }
